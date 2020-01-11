bb6d6f974f6ed0d2c8478dcde2dc1141516d0032
Hamid <mr.hamidsamir@gmail.com>
2020-01-11 23:06:53 +0330

    New Class

--- calculator/src/main/java/artmis/org/calculator/HamidMessage.java
+++ calculator/src/main/java/artmis/org/calculator/HamidMessage.java
@@ -7,7 +7,7 @@ public class HamidMessage {
 
     public void show(Context context,String str){
 
-        Toast.makeText(context,str,Toast.LENGTH_SHORT).show();
+        Toast.makeText(context,"Message =" + str,Toast.LENGTH_SHORT).show();
 
     }
 
