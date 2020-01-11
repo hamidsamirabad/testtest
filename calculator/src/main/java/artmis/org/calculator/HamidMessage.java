0666060a68e06758386622fef48cf288c73393f7
Hamid <mr.hamidsamir@gmail.com>
2020-01-11 23:05:37 +0330

    test2

---
+++ calculator/src/main/java/artmis/org/calculator/HamidMessage.java
@@ -0,0 +1,14 @@
+package artmis.org.calculator;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class HamidMessage {
+
+    public void show(Context context,String str){
+
+        Toast.makeText(context,str,Toast.LENGTH_SHORT).show();
+
+    }
+
+}
